a383f60a3953b5ac19b1e5157e9debfd2ecc5318
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-20 18:27:34 +0530

    Add SQL scripts for creating, dropping, inserting, and selecting quotes

---
+++ data/sql/insertValues.sql
@@ -0,0 +1,8 @@
+INSERT INTO quotes (quote, author)
+VALUES
+    ('The only way to get rid of a temptation is to yield to it.', 'Oscar Wilde'),
+    ('The best way to predict the future is to invent it.', 'Alan Kay'),
+    ('The only way to do great work is to love what you do.', 'Steve Jobs'),
+    ('The only thing we have to fear is fear itself.', 'Franklin D. Roosevelt'),
+    ('The only true wisdom is in knowing you know nothing.', 'Socrates')
+;

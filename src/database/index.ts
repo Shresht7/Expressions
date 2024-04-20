b5ee00720f5e2d81868e3a1f9f6f2b85f6d97f4d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-20 18:27:23 +0530

    Add `SIGINT` and `SIGTERM` handlers to gracefully close the database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,3 +5,13 @@ import { DATABASE_PATH } from '../constants';
 // ------------------------------------------
 export const db = new SQLite3(DATABASE_PATH);
 // ------------------------------------------
+
+process.on('SIGINT', async () => {
+    await db.close();
+    process.exit();
+});
+
+process.on('SIGTERM', async () => {
+    await db.close();
+    process.exit();
+});

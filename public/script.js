94d379037fed24efaafe5ca2b043f2c7d5f82fd7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-23 20:30:15 +0530

    Load quotes all at once

--- public/script.js
+++ public/script.js
@@ -2,7 +2,10 @@
 // SCRIPT
 // ------
 
-// Get a new quote when the page loads
+/** The collection of quotes */
+const quotes = await getQuotes();
+
+// Show a random quote
 getRandomQuote();
 
 // Get a new quote when the user presses the "spacebar" key
@@ -17,22 +20,37 @@ document.body.addEventListener('keyup', (event) => {
 // ----------------
 
 /**
- * Updates the quote and author elements on the page.
- * @param {string} quote - The quote to display.
- * @param {string} author - The author of the quote.
+ * @typedef {Object} Quote
+ * @property {string} quote - The quote
+ * @property {string} author - The author of the quote
+ */
+
+/**
+ * Retrieves the collection of quotes
+ * @returns {Promise<Quote[]>} The collection of quotes
+ */
+async function getQuotes() {
+    const quotes = await fetch(`/quotes.json`)
+        .then(response => response.json())
+    return quotes;
+}
+
+/**
+ * Retrieves a random quote
+ * and updates the quote and author elements on the page
+ */
+async function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const { quote, author } = quotes[randomIndex];
+    updateQuote(quote, author);
+}
+
+/**
+ * Updates the quote and author elements on the page
+ * @param {string} quote - The quote to display
+ * @param {string} author - The author of the quote
  */
 function updateQuote(quote, author) {
     document.getElementById('quote').innerText = quote;
     document.getElementById('quote-author').innerText = author ? "— " + author : "";
-}
-
-/**
- * Retrieves a random quote from the server
- * and updates the quote and author elements on the page.
- */
-async function getRandomQuote() {
-    const { quote, author } = await fetch(`/api/v1/quote/random`)
-        .then(response => response.json())
-
-    updateQuote(quote, author);
 }

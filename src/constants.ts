c8a7a935f9267a06b6fc841b8ae704855d49fc34
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-04 20:00:28 +0530

    Introduce constants for `PRODUCTION_ENVIRONMENT` and `DEVELOPMENT_ENVIRONMENT`

--- src/constants.ts
+++ src/constants.ts
@@ -8,8 +8,14 @@ export const PORT = process.env.PORT || 3000;
 /** The name of the static folder */
 export const STATIC_FOLDER = 'static';
 
+/** Is meant for production */
+export const PRODUCTION_ENVIRONMENT = process.env.NODE_ENV === 'production'
+
+/** Is a development build */
+export const DEVELOPMENT_ENVIRONMENT = process.env.NODE_ENV = 'development'
+
 /** The log level for the logger */
-export const LOG_LEVEL = process.env.LOG_LEVEL ?? process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+export const LOG_LEVEL = process.env.LOG_LEVEL ?? PRODUCTION_ENVIRONMENT ? 'combined' : 'dev';
 
 /** The database path */
 export const DATABASE_PATH = process.env.DATABASE_PATH ?? 'static/quotes.json';
